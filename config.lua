53fea3c13652e117f59ecca349178e1f1448d276
CengizKocer <c-ngiz@live.nl>
2023-03-30 08:55:37 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,17 @@
+Config = {}
+
+-- The duration of the hitman mission in seconds
+Config.MissionDuration = 300
+
+-- The radius within which the player needs to be to start the mission
+Config.StartMissionDistance = 5.0
+
+-- The reward amount for completing the hitman mission
+Config.RewardAmount = 5000
+
+-- The list of gang IDs that hitmen can belong to
+Config.GangList = {
+    "GANG_1",
+    "GANG_2",
+    "GANG_3"
+}
